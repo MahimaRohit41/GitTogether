3361e277f505ad3546020015442ccc757f647b3f
mahimarohit41 <mahimarohit41@gmail.com>
2025-10-09 10:50:27 -0700

    Schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,63 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error("Please add valid Email id")
+            }
+        }
     },
     password: {
         type: String,
+        required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Please enter strong password")
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
+        max: 100
     },
     gender:{
-        type: String
+        type: String,
+        validate(value){ // This works for new document only by default we need to explicitly add to validate for existing document
+            if(!["Male", "Female", "Other"].includes(value)){
+                throw new Error("Please enter valid Gender");
+                
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: '/images/DefaultUserPhoto.jpg'
+    },
+    about: {
+        type: String,
+        default: "This is the default about for the user"
+    },
+    skills: {
+        type: [String]
     }
-});
+},{
+    timestamps: true
+}
+);
 
 module.exports = mongoose.model("User", userSchema);
